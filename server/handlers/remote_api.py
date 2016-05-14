7350898a91438617fcabfecb314c061ba2be1d72
alswl <alswlx@gmail.com>
2016-05-14 17:06:07 +0800

    autostart

--- server/handlers/remote_api.py
+++ server/handlers/remote_api.py
@@ -1,14 +1,17 @@
 # coding=utf-8
 
-from tornado.websocket import WebSocketHandler
+from tornado.websocket import WebSocketHandler, WebSocketClosedError
 
 import play
+from utils import json_util
 
 
 class RemoteWSHandler(WebSocketHandler):
 
     def open(self):
         play.remote_join(self)
+        song = play.get_last_song()
+        play.send_play_command(song, self)
         print("WebSocket opened")
 
     def on_message(self, message):
--- server/play.py
+++ server/play.py
@@ -8,6 +8,9 @@ rooms_handlers = {
     'default': []
 }
 
+last_song = {
+}
+
 
 def remote_join(handler, room='default'):
     if room not in rooms_handlers:
@@ -15,20 +18,39 @@ def remote_join(handler, room='default'):
     rooms_handlers[room].append(handler)
 
 
+def send_play_command(song, handler, room='default'):
+    command = {
+        'action': 'play',
+        'params': {
+            'song': song
+        }
+    }
+    try:
+        handler.write_message(json_util.format_value(command))
+    except websocket.WebSocketClosedError:
+        rooms_handlers[room].remove(handler)
+
+
 def play_song(song, room='default'):
     for handler in rooms_handlers.get(room, []):
-        command = {
-            'action': 'play',
-            'params': {
-                'song': song
-            }
-        }
-        try:
-            handler.write_message(json_util.format_value(command))
-        except websocket.WebSocketClosedError:
-            rooms_handlers[room].remove(handler)
+        send_play_command(room=room, song=song, handler=handler)
+
+    last_song[room] = song
     return len(rooms_handlers.get(room, []))
 
 
 def get_online_count(room='default'):
     return len(rooms_handlers.get(room, []))
+
+
+def heartbeat(room='default'):
+    for handler in rooms_handlers.get(room, []):
+        try:
+            handler.write_message('ping')
+        except websocket.WebSocketClosedError:
+            rooms_handlers[room].remove(handler)
+    return len(rooms_handlers.get(room, []))
+
+
+def get_last_song(room='default'):
+    return last_song.get(room, '')
